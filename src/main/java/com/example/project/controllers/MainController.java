3c54eaeb0554aa29ab6a19f9b6d2259d64e08b4c
Egor Elinev <elinevskijegor@gmail.com>
2025-02-16 21:53:36 +0300

    fix

--- src/main/java/com/example/project/controllers/MainController.java
+++ src/main/java/com/example/project/controllers/MainController.java
@@ -9,10 +9,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class MainController {
     // GET с Query Parameters
     // Пример запроса: /api/cars?brand=Toyota&model=Camry
+    public static String strBrand = "brand";
+    public static String strModel = "model";
+    public static String strPrice = "price";
+    public static String strYear = "year";
+    public static String sameBrand = "Toyota";
 
     @GetMapping("/api/cars")
     public Map<String, Object> getCars(
@@ -21,17 +27,17 @@ public class MainController {
 
         Map<String, Object> car1 = new HashMap<>();
         car1.put("id", 1);
-        car1.put("brand", "Toyota");
-        car1.put("model", "Camry");
-        car1.put("year", 2020);
-        car1.put("price", 25000);
+        car1.put(strBrand, sameBrand);
+        car1.put(strModel, "Camry");
+        car1.put(strYear, 2020);
+        car1.put(strPrice, 25000);
 
         Map<String, Object> car2 = new HashMap<>();
         car2.put("id", 2);
-        car2.put("brand", "Toyota");
-        car2.put("model", "Corolla");
-        car2.put("year", 2019);
-        car2.put("price", 20000);
+        car2.put(strBrand, sameBrand);
+        car2.put(strModel, "Corolla");
+        car2.put(strYear, 2019);
+        car2.put(strPrice, 20000);
 
         List<Map<String, Object>> sameCars = new ArrayList<>();
 
@@ -60,18 +66,18 @@ public class MainController {
 
         Map<String, Object> car1 = new HashMap<>();
         car1.put("id", 1);
-        car1.put("brand", "Toyota");
-        car1.put("model", "Land Cruiser");
-        car1.put("year", 2020);
-        car1.put("price", 25000);
+        car1.put(strBrand, sameBrand);
+        car1.put(strModel, "Land Cruiser");
+        car1.put(strYear, 2020);
+        car1.put(strPrice, 25000);
         car1.put("description", "Комфортный седан с отличной управляемостью.");
 
         Map<String, Object> car2 = new HashMap<>();
         car2.put("id", 2);
-        car2.put("brand", "BMW");
-        car2.put("model", "X5");
-        car2.put("year", 2022);
-        car2.put("price", 50000);
+        car2.put(strBrand, "BMW");
+        car2.put(strModel, "X5");
+        car2.put(strYear, 2022);
+        car2.put(strPrice, 50000);
         car2.put("description", "среднеразмерный кроссовер от немецкого автопроизводителя BMW");
 
         List<Map<String, Object>> cars = new ArrayList<>();
