bcea483e03a7ac0299bfce2a261a2e84fcddb3ed
Egor Elinev <elinevskijegor@gmail.com>
2025-02-16 23:00:29 +0300

    fix

--- src/main/java/com/example/project/controllers/MainController.java
+++ src/main/java/com/example/project/controllers/MainController.java
@@ -14,35 +14,56 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     // GET с Query Parameters
     // Пример запроса: /api/cars?brand=Toyota&model=Camry
-    public static String strBrand = "brand";
-    public static String strModel = "model";
-    public static String strPrice = "price";
-    public static String strYear = "year";
-    public static String sameBrand = "Toyota";
+    private static final Map<String, Object> car1 = new HashMap<>();
+    private static final Map<String, Object> car2 = new HashMap<>();
+    private static final Map<String, Object> car3 = new HashMap<>();
+    private static final Map<String, Object> car4 = new HashMap<>();
 
-    @GetMapping("/api/cars")
-    public Map<String, Object> getCars(
-             @RequestParam(required = false) String brand,
-             @RequestParam(required = false) String model) {
+    public MainController() {
+        String strBrand = "brand";
+        String strModel = "model";
+        String strPrice = "price";
+        String strYear = "year";
+        String sameBrand = "Toyota";
 
-        Map<String, Object> car1 = new HashMap<>();
         car1.put("id", 1);
         car1.put(strBrand, sameBrand);
         car1.put(strModel, "Camry");
         car1.put(strYear, 2020);
         car1.put(strPrice, 25000);
 
-        Map<String, Object> car2 = new HashMap<>();
         car2.put("id", 2);
         car2.put(strBrand, sameBrand);
         car2.put(strModel, "Corolla");
         car2.put(strYear, 2019);
         car2.put(strPrice, 20000);
 
+        car3.put("id", 3);
+        car3.put(strBrand, sameBrand);
+        car3.put(strModel, "Land Cruiser");
+        car3.put(strYear, 2020);
+        car3.put(strPrice, 25000);
+        car3.put("description", "Комфортный седан с отличной управляемостью.");
+
+        car4.put("id", 4);
+        car4.put(strBrand, "BMW");
+        car4.put(strModel, "X5");
+        car4.put(strYear, 2022);
+        car4.put(strPrice, 50000);
+        car4.put("description", "среднеразмерный кроссовер от немецкого автопроизводителя BMW");
+    }
+
+    @GetMapping("/api/cars")
+    public Map<String, Object> getCars(
+             @RequestParam(required = false) String brand,
+             @RequestParam(required = false) String model) {
+
         List<Map<String, Object>> sameCars = new ArrayList<>();
 
         sameCars.add(car1);
         sameCars.add(car2);
+        sameCars.add(car3);
+        sameCars.add(car4);
 
         // Фильтрация по brand и model (если указаны)
         if (brand != null) {
@@ -64,25 +85,11 @@ public class MainController {
     @GetMapping("/api/cars/{id}")
     public Map<String, Object> getCarById(@PathVariable int id) {
 
-        Map<String, Object> car1 = new HashMap<>();
-        car1.put("id", 1);
-        car1.put(strBrand, sameBrand);
-        car1.put(strModel, "Land Cruiser");
-        car1.put(strYear, 2020);
-        car1.put(strPrice, 25000);
-        car1.put("description", "Комфортный седан с отличной управляемостью.");
-
-        Map<String, Object> car2 = new HashMap<>();
-        car2.put("id", 2);
-        car2.put(strBrand, "BMW");
-        car2.put(strModel, "X5");
-        car2.put(strYear, 2022);
-        car2.put(strPrice, 50000);
-        car2.put("description", "среднеразмерный кроссовер от немецкого автопроизводителя BMW");
-
         List<Map<String, Object>> cars = new ArrayList<>();
         cars.add(car1);
         cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
 
         if (id != 0) {
             cars.removeIf(car -> !car.get("id").equals(id));
