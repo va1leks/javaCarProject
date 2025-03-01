2fcdc4efad5a45b919c1b7fbacf5cf8166e6230f
Egor Elinev <elinevskijegor@gmail.com>
2025-03-01 20:01:06 +0300

    add exception

--- src/main/java/com/example/project/exeption/DefaultAdvice.java
+++ src/main/java/com/example/project/exeption/DefaultAdvice.java
@@ -1,9 +1,7 @@
-package com.example.project.advice;
+package com.example.project.exeption;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-import com.example.project.exeption.CarNotFoundException;
-import com.example.project.exeption.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
