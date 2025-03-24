8b31884bf07606080179017079397cfb50660ca7
Egor Elinev <elinevskijegor@gmail.com>
2025-03-24 10:30:10 +0300

    fix

--- src/main/java/com/example/project/exeption/ErrorMessages.java
+++ src/main/java/com/example/project/exeption/ErrorMessages.java
@@ -1,6 +1,10 @@
 package com.example.project.exeption;
 
 public class ErrorMessages {
+    private ErrorMessages() {
+        throw new UnsupportedOperationException(
+                "This is a utility class and cannot be instantiated");
+    }
     public static final String CAR_NOT_FOUND = "Car with id %d not found";
     public static final String USER_NOT_FOUND = "User with id %d not found";
     public static final String DEALERSHIP_NOT_FOUND = "Dealership with id %d not found";
