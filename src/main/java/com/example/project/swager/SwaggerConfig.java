5a00f1cdbc2bddbb722563bbaa6d2271a90d34cd
Egor Elinev <elinevskijegor@gmail.com>
2025-03-31 09:48:24 +0300

    add log

---
+++ src/main/java/com/example/project/swager/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.example.project.swager;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Dealership Application API",
+                description = "API для приложения Dealership,"
+                        + " позволяющего пользователям покупать и машины.",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "va1leks",
+                        email = "elinevskijegor@gmail.com",
+                        url = "https://github.com/va1leks"
+                )
+        )
+)
+public class SwaggerConfig {
+}
