327d2c0a2a832fec1f57fd172b601b69ed341656
Egor Elinev <elinevskijegor@gmail.com>
2025-04-11 17:48:41 +0300

    fix

--- src/main/java/com/example/project/controllers/LogController.java
+++ src/main/java/com/example/project/controllers/LogController.java
@@ -1,7 +1,9 @@
 package com.example.project.controllers;
 
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -11,13 +13,17 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/logs")
 public class LogController {
 
-    private static final DateTimeFormatter LOG_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter LOG_DATE_FORMAT =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @GetMapping("/{date}")
     public ResponseEntity<Resource> getLogsByDate(@PathVariable String date) throws IOException {
@@ -54,7 +60,8 @@ public class LogController {
 
         Resource resource = new UrlResource(tempLogFile.toUri());
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=logs-" + date + ".log")
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=logs-" + date + ".log")
                 .contentType(MediaType.TEXT_PLAIN)
                 .body(resource);
     }
